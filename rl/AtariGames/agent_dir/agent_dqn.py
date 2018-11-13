260a57bed0c69aa0a222688b21186c5e2f4a0af6
Leon <chin340823@163.com>
2018-11-13 18:06:22 +0800

    update agent_dqn

--- rl/AtariGames/agent_dir/agent_dqn.py
+++ rl/AtariGames/agent_dir/agent_dqn.py
@@ -1,5 +1,6 @@
 from agent_dir.agent import Agent
 import tensorflow as tf
+from collections import deque
 
 
 class DQN:
@@ -7,6 +8,7 @@ class DQN:
     def __init__(self):
         self.n_obs = [80, 80, 4]
         self.n_actions = 3
+        self.gamma = 0.9    # discount factor
         self._build()
 
     def _build(self):
@@ -19,20 +21,32 @@ class DQN:
         self.states = tf.placeholder(dtype=tf.float32, shape=[None, 80, 80, 4], name="states")
         self.actions = tf.placeholder(dtype=tf.float32, shape=[None, self.n_actions], name="actions")
         self.rewards = tf.placeholder(dtype=tf.float32, shape=[None, 1], name="rewards")
+        self.next_states = tf.placeholder(dtype=tf.float32, shape=[None, 80, 80, 4], name="next_states")
+        
+        self.q_target = tf.placeholder(dtype=tf.float32, shape=[None, self.n_actions], name="q_target")
 
 
     def _add_predictions(self):
+        with tf.variable_scope("eval_net"):
+            self.output_eval = self.predict_op(self.states, name="output_eval")
+        with tf.variable_scope("target_net"):
+            self.output_target = self.predict_op(self.next_states, name="output_target")
+        
+        self.loss = tf.reduce_mean(tf.square(self.output_eval - self.q_target))
+
+
+    def predict_op(self, input, name):
         with tf.variable_scope("conv-1"):
             filter1 = tf.get_variable(name="filter1", shape=[3, 3, 4, 64], 
-                                             initializer=tf.contrib.layers.xavier_initializer())
-            out_conv1 = tf.nn.conv2d(input=self.states, filter=filter1, 
+                                      initializer=tf.contrib.layers.xavier_initializer())
+            out_conv1 = tf.nn.conv2d(input=input, filter=filter1, 
                                      strides=[1, 2, 2, 1], padding="SAME")
             out_pool1 = tf.nn.max_pool(value=out_conv1, ksize=[1, 2, 2, 1], 
                                        strides=[1, 2, 2, 1], padding="SAME")
 
         with tf.variable_scope("conv-2"):
             filter2 = tf.get_variable(name="filter2", shape=[3, 3, 64, 64], 
-                                             initializer=tf.contrib.layers.xavier_initializer())
+                                      initializer=tf.contrib.layers.xavier_initializer())
             out_conv2 = tf.nn.conv2d(input=out_pool1, filter=filter2, 
                                      strides=[1, 2, 2, 1], padding="SAME")
             out_pool2 = tf.nn.max_pool(value=out_conv2, ksize=[1, 2, 2, 1], 
@@ -43,10 +57,16 @@ class DQN:
             weights = tf.get_variable(name="weights", shape=[flatten_vec.shape[-1], self.n_actions], 
                                       initializer=tf.contrib.layers.xavier_initializer())
             bias = tf.get_variable(name="bias", shape=[self.n_actions], initializer=tf.zeros_initializer())
-            self.output = tf.matmul(flatten_vec, weights) + bias
+            output = tf.add(tf.matmul(flatten_vec, weights), bias, name=name)
+
+        return output
 
 
     def _add_saver(self):
+        pass
+
+
+    def train(self, session):
         pass
 
 
@@ -87,10 +107,28 @@ class Agent_DQN(Agent):
         """
         Implement your training algorithm here
         """
-        ##################
-        # YOUR CODE HERE #
-        ##################
-        pass
+        tf.logging.info("...... start training ......")
+        self.init_game_setting()
+        i_episode = -1
+        running_reward = None
+        
+        replay_buf = []
+        replay_buf = deque(maxlen=10)
+
+        while True:
+            state = self.env.reset()
+            done = False
+
+            #playing one game
+            while(not done):
+                self.env.env.render()
+                action = self.make_action(state, test=False)
+                next_state, reward, done, info = self.env.step(action)
+                replay_buf.append([state, action, reward, next_state])
+                state = next_state
+
+            print("replay_reward:", [(rep[1], rep[2]) for rep in replay_buf])
+            # self.model.train(self.sess, episode_states, episode_actions, discount_rewards)
 
 
     def make_action(self, observation, test=True):
